9d1806c083ff4e0d013ea1c25f2fa689fac94bc7
bezael <bezael@gmail.com>
2025-05-19 22:31:07 +0200

    Refactor app layout and update routing to include start screen redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'start',
+    pathMatch: 'full',
+  },
+  {
+    path: 'start',
+    loadComponent: () => import('./features/start-screen/start-screen.component').then(m => m.StartScreenComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
