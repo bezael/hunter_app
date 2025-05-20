bc93701f767ef65b6fe45d1f8d339cb4d8d843d5
bezael <bezael@gmail.com>
2025-05-20 12:24:21 +0200

    Add StoreService to manage game state, including board updates, player position, orientation, arrows, and game status management.

---
+++ src/app/shared/services/store/store.service.ts
@@ -0,0 +1,49 @@
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { GameBoardService } from '@app/shared/services/game-board.service';
+import { GameState, INITIAL_STATE, Orientation, Position, Status } from '@shared/services/store/store.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StoreService {
+  readonly board = computed(() => this.state().board);
+  readonly player = computed(() => this.state().player);
+  readonly gameStatus = computed(() => this.state().gameStatus);
+
+  private readonly state = signal<GameState>(INITIAL_STATE);
+  private readonly _gameBoardService = inject(GameBoardService);
+
+  updateBoard(boardSize: number) {
+    const newBoard = this._gameBoardService.createBoard(boardSize);
+    this.state.update(state => ({ ...state, board: newBoard }));
+  }
+
+  updatePlayerPosition(position: Position = { x: 0, y: 0 }) {
+    this.state.update(state => ({
+      ...state,
+      player: { ...state.player, position },
+    }));
+  }
+
+  updatePlayerOrientation(orientation: Orientation = 'NORTH') {
+    this.state.update(state => ({
+      ...state,
+      player: { ...state.player, orientation },
+    }));
+  }
+
+  updateArrows(arrows: number) {
+    this.state.update(state => ({
+      ...state,
+      player: { ...state.player, arrows },
+    }));
+  }
+
+  updateGameStatus(status: Status) {
+    this.state.update(state => ({ ...state, gameStatus: status }));
+  }
+
+  resetGame() {
+    this.state.set(INITIAL_STATE);
+  }
+}
