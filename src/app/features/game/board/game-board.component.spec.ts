992f29e0851387dcce12a30a143b212acd2eaeee
bezael <bezael@gmail.com>
2025-05-20 16:34:48 +0200

    Add data-testid attributes to GameBoardComponent and update unit tests to utilize a mock store service

--- src/app/features/game/board/game-board.component.spec.ts
+++ src/app/features/game/board/game-board.component.spec.ts
@@ -1,23 +1,64 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { signal } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { GameBoardComponent } from '@app/features/game/board/game-board.component';
+import { StoreService } from '@app/shared/services/store/store.service';
+import { render, screen } from '@testing-library/angular';
 
-import { GameBoardComponent } from './game-board.component';
+class MockStoreService {
+  board = signal<string[][]>([]);
+  player = signal<string>('');
+  gameStatus = signal<string>('');
 
+  setBoard(newBoard: string[][]) {
+    this.board.set(newBoard);
+  }
+  setPlayer(newPlayer: string) {
+    this.player.set(newPlayer);
+  }
+  setGameStatus(newStatus: string) {
+    this.gameStatus.set(newStatus);
+  }
+}
+
+const mockRouter = {
+  navigate: jest.fn(),
+};
 describe('GameBoardComponent', () => {
-  let component: GameBoardComponent;
-  let fixture: ComponentFixture<GameBoardComponent>;
+  let mockStoreService: MockStoreService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [GameBoardComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(GameBoardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    mockStoreService = new MockStoreService();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('render component and display initial board', async () => {
+    const initialBoard = [
+      ['', '', ''],
+      ['', 'G', ''],
+      ['', '', 'P'],
+    ];
+
+    mockStoreService.setBoard(initialBoard);
+
+    await render(GameBoardComponent, {
+      providers: [{ provide: StoreService, useValue: mockStoreService }],
+    });
+
+    const container = screen.getByTestId('game-board');
+    expect(container).toBeInTheDocument();
+
+    const rows = screen.getAllByTestId(/board-row-/g);
+    expect(rows.length).toBe(3);
+
+    const cells = screen.getAllByTestId(/board-cell-/g);
+    expect(cells.length).toBe(9);
+  });
+
+  it('navigate to /start if board is empty', async () => {
+    await render(GameBoardComponent, {
+      providers: [{ provide: Router, useValue: mockRouter }],
+    });
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/start']);
   });
 });
