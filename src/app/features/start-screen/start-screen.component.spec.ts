6063fa3874e140392d515d17e257d088ad58c216
bezael <bezael@gmail.com>
2025-05-20 19:34:58 +0200

    Add unit tests for StartScreenComponent

--- src/app/features/start-screen/start-screen.component.spec.ts
+++ src/app/features/start-screen/start-screen.component.spec.ts
@@ -1,9 +1,80 @@
+import { Router } from '@angular/router';
+import { StartScreenComponent } from '@app/features/start-screen/start-screen.component';
+import { StoreService } from '@app/shared/services/store/store.service';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
 
+class MockStoreService {
+  resetGame = jest.fn();
+  updateArrows = jest.fn();
+  updateBoard = jest.fn();
+}
+
+const mockRouter = {
+  navigate: jest.fn(),
+};
 
 describe('StartScreenComponent', () => {
-  it.todo('initialize form with default values')
+  let mockStoreService: MockStoreService;
 
-  it.todo('disable start button when form is invalid')
+  beforeEach(() => {
+    mockStoreService = new MockStoreService();
+  });
 
-  it.todo('navigate to game when form is valid')
+  it('initialize form with default values', async () => {
+    const user = userEvent.setup();
+
+    await render(StartScreenComponent);
+
+    const inputBoardSize = screen.getByLabelText('Board size (n x n):');
+    user.type(inputBoardSize, '4');
+
+    const inputNumWells = screen.getByLabelText('Number of wells:');
+    user.type(inputNumWells, '3');
+
+    const inputNumArrows = screen.getByLabelText('Number of arrows:');
+    user.type(inputNumArrows, '1');
+
+    const startGameButton = screen.getByRole('button', { name: /start game/i });
+
+    expect(screen.getByRole('heading', { name: /hunt the wumpus/i })).toBeInTheDocument();
+    expect(startGameButton).toBeEnabled();
+  });
+
+  it('disable start button when form is invalid', async () => {
+    const user = userEvent.setup();
+    await render(StartScreenComponent);
+
+    const inputBoardSize = screen.getByLabelText('Board size (n x n):');
+    await user.type(inputBoardSize, '4');
+
+    const inputNumWells = screen.getByLabelText('Number of wells:');
+    await user.type(inputNumWells, '0');
+
+    const inputNumArrows = screen.getByLabelText('Number of arrows:');
+    await user.type(inputNumArrows, '1');
+
+    const startGameButton = screen.getByRole('button', { name: /start game/i });
+
+    expect(startGameButton).toBeDisabled();
+  });
+
+  it('navigate to game when form is valid', async () => {
+    const user = userEvent.setup();
+
+    await render(StartScreenComponent, {
+      providers: [
+        { provide: Router, useValue: mockRouter },
+        { provide: StoreService, useValue: mockStoreService },
+      ],
+    });
+
+    const startGameButton = screen.getByRole('button', { name: /start game/i });
+    await user.click(startGameButton);
+
+    expect(mockStoreService.updateBoard).toHaveBeenCalledWith(4);
+    expect(mockStoreService.updateArrows).toHaveBeenCalledWith(1);
+
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/game']);
+  });
 });
