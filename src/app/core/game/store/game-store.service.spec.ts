a891c1a2b099a5d4304780c59182a3725f3b6bdb
bezael <bezael@gmail.com>
2025-05-21 14:53:32 +0200

    test: update tests for GameStoreService to reflect new structure

--- src/app/core/game/store/game-store.service.spec.ts
+++ src/app/core/game/store/game-store.service.spec.ts
@@ -1,21 +1,23 @@
 import { TestBed } from '@angular/core/testing';
-import { GameBoardService } from '@app/core/game/board/game-board.service';
-import { INITIAL_STATE, Position } from '@shared/services/store/store.model';
-import { StoreService } from './store.service';
+import { GameBoardService } from '../board/game-board.service';
+
+import { INITIAL_STATE } from '../constants';
+import { GameState, Position } from '../types';
+import { GameStoreService } from './game-store.service';
 
 const mockGameBoardService = {
   createBoard: jest.fn(),
 };
 
-describe('StoreService', () => {
-  let service: StoreService;
+describe('GameStoreService', () => {
+  let service: GameStoreService;
   let gameBoardService: jest.Mocked<GameBoardService>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [{ provide: GameBoardService, useValue: mockGameBoardService }],
     });
-    service = TestBed.inject(StoreService);
+    service = TestBed.inject(GameStoreService);
     gameBoardService = TestBed.inject(GameBoardService) as jest.Mocked<GameBoardService>;
   });
 
@@ -24,25 +26,35 @@ describe('StoreService', () => {
   });
 
   it('initialize game state with default values', () => {
-    const initialPlayer = {
-      position: { x: 0, y: 0 },
-      orientation: 'NORTH',
-      arrows: 3,
+    const initialGameState: GameState = {
+      board: [],
+      player: {
+        position: { x: 0, y: 0 },
+        direction: 'NORTH',
+        arrows: 3,
+        isAlive: true,
+      },
+      gameStatus: 'PLAYING',
+      wumpus: {
+        position: { x: 0, y: 0 },
+        alive: false,
+      },
     };
-    expect(service.board()).toEqual([]);
-    expect(service.player()).toEqual(initialPlayer);
-    expect(service.gameStatus()).toBe('PLAYING');
+
+    expect(
+      service.updateFromAction({ newState: initialGameState, perceptions: ['BREEZE'], gameStatus: 'PLAYING' })
+    ).toEqual([]);
   });
 
-  it('create a new board with the specified size and update the state', () => {
+  fit('create a new board with the specified size and update the state', () => {
     const boardSize = 4;
     const mockBoard = Array(boardSize).fill(Array(boardSize).fill(''));
     gameBoardService.createBoard.mockReturnValue(mockBoard);
 
     service.updateBoard(boardSize);
 
-    expect(service.board().length).toBe(boardSize);
-    expect(service.board()[0].length).toBe(boardSize);
+    expect(service.state().board.length).toBe(boardSize);
+    expect(service.state().board[0].length).toBe(boardSize);
     expect(gameBoardService.createBoard).toHaveBeenCalledWith(boardSize);
   });
 
@@ -50,8 +62,8 @@ describe('StoreService', () => {
     const newPosition: Position = { x: 4, y: 3 };
     service.updatePlayerPosition(newPosition);
 
-    expect(service.player().position).toEqual(newPosition);
-    expect(service.player().arrows).toEqual(INITIAL_STATE.player.arrows);
+    expect(service.state().player.position).toEqual(newPosition);
+    expect(service.state().player.arrows).toEqual(INITIAL_STATE.player.arrows);
   });
 
   it('create a new board using GameBoardService and verify the service call', () => {
