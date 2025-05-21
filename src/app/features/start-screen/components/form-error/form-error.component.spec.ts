722a23a6a477b23204e29c70316462b03d9bca47
bezael <bezael@gmail.com>
2025-05-21 08:40:06 +0200

    Add unit tests for FormErrorComponent

--- src/app/features/start-screen/components/form-error/form-error.component.spec.ts
+++ src/app/features/start-screen/components/form-error/form-error.component.spec.ts
@@ -1,13 +1,48 @@
+import { render, screen } from '@testing-library/angular';
+import { FormErrorComponent } from './form-error.component';
 
 describe('FormErrorComponent', () => {
- 
+  it('not show error message when show is false', async () => {
+    await render(FormErrorComponent, {
+      inputs: {
+        show: false,
+        message: 'Test error message',
+        fieldId: 'test-field',
+      },
+    });
 
-  it.todo('not show error message when show is false')
-  
-  it.todo('show error message when show is true')
-    
+    const errorMessageAlert = screen.queryByRole('alert');
+    expect(errorMessageAlert).not.toBeInTheDocument();
+  });
 
-  it.todo('update error message when message input changes');
+  it('show error message when show is true', async () => {
+    await render(FormErrorComponent, {
+      inputs: {
+        show: true,
+        message: 'Test error message',
+        fieldId: 'test-field',
+      },
+    });
 
+    const errorMessageAlert = screen.getByRole('alert');
+    expect(errorMessageAlert).toBeInTheDocument();
+    expect(errorMessageAlert).toHaveTextContent('Test error message');
+  });
 
+  it('update error message when message input changes', async () => {
+    const { fixture } = await render(FormErrorComponent, {
+      inputs: {
+        show: true,
+        message: 'Initial error message',
+        fieldId: 'test-field',
+      },
+    });
+    const errorMessageAlert = screen.getByRole('alert');
+    expect(errorMessageAlert).toHaveTextContent('Initial error message');
+
+    fixture.componentRef.setInput('message', 'Updated error message');
+    fixture.detectChanges();
+
+    expect(errorMessageAlert).toHaveTextContent('Updated error message');
+  });
 });
