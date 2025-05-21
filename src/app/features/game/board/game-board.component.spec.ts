aaf89108f1a6f1e577ec3d39efa607b9e3e79f52
bezael <bezael@gmail.com>
2025-05-21 14:48:59 +0200

    (test): refactor GameBoardComponent tests

--- src/app/features/game/board/game-board.component.spec.ts
+++ src/app/features/game/board/game-board.component.spec.ts
@@ -1,63 +1,63 @@
-import { signal } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
+import { INITIAL_STATE } from '@app/core/game/constants';
 import { GameBoardComponent } from '@app/features/game/board/game-board.component';
-import { StoreService } from '@app/shared/services/store/store.service';
+import { GameStoreService } from '@core/game/store/game-store.service';
 import { render, screen } from '@testing-library/angular';
 
-class MockStoreService {
-  board = signal<string[][]>([]);
-  player = signal<string>('');
-  gameStatus = signal<string>('');
+const fakeInitialBoard = [
+  ['', '', '', ''],
+  ['', 'G', '', ''],
+  ['', '', '', 'P'],
+  ['', '', '', ''],
+];
 
-  setBoard(newBoard: string[][]) {
-    this.board.set(newBoard);
-  }
-  setPlayer(newPlayer: string) {
-    this.player.set(newPlayer);
-  }
-  setGameStatus(newStatus: string) {
-    this.gameStatus.set(newStatus);
-  }
-}
+const mockGameStoreService = {
+  state: jest.fn().mockReturnValue({
+    ...INITIAL_STATE,
+    board: [...fakeInitialBoard],
+  }),
+};
 
 const mockRouter = {
   navigate: jest.fn(),
 };
-describe('GameBoardComponent', () => {
-  let mockStoreService: MockStoreService;
 
+describe('GameBoardComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    mockStoreService = new MockStoreService();
   });
 
   it('render component and display initial board', async () => {
-    const initialBoard = [
-      ['', '', ''],
-      ['', 'G', ''],
-      ['', '', 'P'],
-    ];
-
-    mockStoreService.setBoard(initialBoard);
-
     await render(GameBoardComponent, {
-      providers: [{ provide: StoreService, useValue: mockStoreService }],
+      providers: [
+        { provide: GameStoreService, useValue: mockGameStoreService },
+        { provide: Router, useValue: mockRouter },
+      ],
     });
 
     const container = screen.getByTestId('game-board');
     expect(container).toBeInTheDocument();
 
     const rows = screen.getAllByTestId(/board-row-/i);
-    expect(rows.length).toBe(3);
+    expect(rows.length).toBe(4);
 
     const cells = screen.getAllByTestId(/board-cell-/i);
-    expect(cells.length).toBe(9);
+    expect(cells.length).toBe(16);
   });
 
   it('navigate to /start if board is empty', async () => {
+    const emptyBoardMock = {
+      state: jest.fn().mockReturnValue({
+        ...INITIAL_STATE,
+      }),
+    };
+
     await render(GameBoardComponent, {
-      providers: [{ provide: Router, useValue: mockRouter }],
+      providers: [
+        { provide: GameStoreService, useValue: emptyBoardMock },
+        { provide: Router, useValue: mockRouter },
+      ],
     });
     expect(mockRouter.navigate).toHaveBeenCalledWith(['/start']);
   });
