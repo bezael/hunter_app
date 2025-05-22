34add365ce6322c8733fc62a8fc1b8dec0386171
bezael <bezael@gmail.com>
2025-05-22 07:32:04 +0200

    refactor: rename StoreService to GameStoreService and remove unused methods

--- src/app/core/game/store/game-store.service.ts
+++ src/app/core/game/store/game-store.service.ts
@@ -1,50 +1,44 @@
 import { computed, inject, Injectable, signal } from '@angular/core';
 import { GameBoardService } from '@app/core/game/board/game-board.service';
-import { Direction, GameState, GameStatus, Position } from '@core/game/types';
-import { INITIAL_STATE } from './store.model';
+import { INITIAL_STATE } from '../constants';
+import { GameActionResult, GameState, GameStatus, Perception } from '../types';
 
 @Injectable({
   providedIn: 'root',
 })
-export class StoreService {
-  readonly board = computed(() => this.state().board);
-  readonly player = computed(() => this.state().player);
-  readonly gameStatus = computed(() => this.state().gameStatus);
+export class GameStoreService {
+  readonly state = computed(() => this._state());
+  readonly gameStatus = computed(() => this._status());
+  readonly currentPerceptions = computed(() => this._perceptions());
 
-  private readonly state = signal<GameState>(INITIAL_STATE);
+  private readonly _state = signal<GameState>(INITIAL_STATE);
+  private readonly _status = signal<GameStatus>('PLAYING');
+  private readonly _perceptions = signal<Perception[]>([]);
+
   private readonly _gameBoardService = inject(GameBoardService);
 
-  updateBoard(boardSize: number) {
-    const newBoard = this._gameBoardService.createBoard(boardSize);
-    this.state.update(state => ({ ...state, board: newBoard }));
+  updateFromAction(result: GameActionResult) {
+    const { newState, perceptions, gameStatus } = result;
+    this._state.set(newState);
+    this._perceptions.set(perceptions);
+    this._status.set(gameStatus);
   }
 
-  updatePlayerPosition(position: Position = { x: 0, y: 0 }) {
-    this.state.update(state => ({
-      ...state,
-      player: { ...state.player, position },
-    }));
-  }
-
-  updatePlayerOrientation(direction: Direction = 'NORTH') {
-    this.state.update(state => ({
-      ...state,
-      player: { ...state.player, direction },
-    }));
+  updateBoard(boardSize: number, numWells: number) {
+    const newBoard = this._gameBoardService.createBoard(boardSize, numWells);
+    this._state.update(state => ({ ...state, board: newBoard }));
   }
 
   updateArrows(arrows: number) {
-    this.state.update(state => ({
+    this._state.update(state => ({
       ...state,
       player: { ...state.player, arrows },
     }));
   }
 
-  updateGameStatus(status: GameStatus) {
-    this.state.update(state => ({ ...state, gameStatus: status }));
-  }
-
   resetGame() {
-    this.state.set(INITIAL_STATE);
+    this._state.set(INITIAL_STATE);
+    this._perceptions.set([]);
+    this._status.set('PLAYING');
   }
 }
