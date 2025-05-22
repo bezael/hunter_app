35837e90460929dfae8a7418bb6a20f6cabfb681
bezael <bezael@gmail.com>
2025-05-22 22:29:11 +0200

    refactor: remove GameOutputComponent and associated files

--- src/app/features/game/output/game-output.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-game-output',
-  imports: [],
-  templateUrl: './game-output.component.html',
-  styleUrl: './game-output.component.scss',
-})
-export class GameOutputComponent {}
