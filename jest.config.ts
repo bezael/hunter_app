442ff8505b61a1aae1190af05d809026c2cb0782
bezael <bezael@gmail.com>
2025-05-22 20:48:33 +0200

    refactor: remove shared path aliases from Jest and TypeScript configuration

--- jest.config.ts
+++ jest.config.ts
@@ -9,7 +9,6 @@ const config: Config = {
     '@app/(.*)$': '<rootDir>/src/app/$1',
     '@core/(.*)$': '<rootDir>/src/app/core/$1',
     '@features/(.*)$': '<rootDir>/src/app/features/$1',
-    '@shared/(.*)$': '<rootDir>/src/app/shared/$1',
   },
 };
 
